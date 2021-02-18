da80c8cc50e21de57f8a824ae693333d2eefcb7b
Skylord <skylord@skylord.local>
2021-02-18 06:12:58 +0530

    Added readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Read CSV File
+
+### Running the script:
+- Go to the root folder of the project and run `npm run app`. 
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare const csv: any;
-declare const fs: any;
-declare const request: any;
-declare const chalk: any;
-declare let count: number;
-declare let fileSize: number;
